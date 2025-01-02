c413274824a1e00bab9330259df1a0c1a408359c
Christos Stamoulos <147555306+ChristosStamoulos@users.noreply.github.com>
2025-01-02 17:43:03 +0200

    added player

---
+++ Player/PlayerLocomotion.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+   
+    private InputManager inputManager;
+    private Transform cameraObject;
+    private Rigidbody player;
+
+    private Animator animator;
+   
+    public float movementSpeed = 7f;
+    public float rotationSpeed = 15f;
+
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        inputManager = GetComponent<InputManager>();
+        player = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+
+        if (player == null)
+            Debug.LogError("Rigidbody component not found on " + gameObject.name);
+    }
+
+    public void HandlePlayer()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection += cameraObject.right * inputManager.horizontalInput;
+        moveDirection.y = 0f;
+
+        if (moveDirection.sqrMagnitude > 0.01f) 
+        {
+            animator.SetBool("IsMoving", true);
+            moveDirection.Normalize();
+            moveDirection *= movementSpeed;
+            player.velocity = new Vector3(moveDirection.x, player.velocity.y, moveDirection.z);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+            player.velocity = new Vector3(0, player.velocity.y, 0);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = cameraObject.forward * inputManager.verticalInput +
+                                  cameraObject.right * inputManager.horizontalInput;
+
+        targetDirection.y = 0f;
+
+        if (targetDirection.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection.normalized);
+            Quaternion smoothedRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = smoothedRotation;
+        }
+    }
+}
---
+++ Player/PlayerManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+   
+        InputManager inputManager;
+        PlayerLocomotion playerLocomotion;
+
+        private void Awake()
+        {
+            inputManager = GetComponent<InputManager>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+        }
+
+        private void Update()
+        {
+            inputManager.HandleInputs();
+        }
+
+        private void FixedUpdate()
+        {
+            playerLocomotion.HandlePlayer();
+        }
+    
+
+
+}
